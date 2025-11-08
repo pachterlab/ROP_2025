0539340db7094694c3556780f98366af2cf715f9
josephrich98 <josephrich98@gmail.com>
2025-11-08 00:19:33 -0800

    initialized

--- tests/testthat/setup.R
+++
@@ -1,6 +0,0 @@
-# if miniconda not found or environment not found, then prompt setup_python_env
-if ((!file.exists(reticulate::conda_binary()) || (!("wompwomp_env" %in% reticulate::conda_list()$name)))) {
-    wompwomp::setup_python_env()
-}
-reticulate::use_condaenv("wompwomp_env", required = TRUE)
-Sys.setenv(RETICULATE_PYTHON = reticulate::py_config()$python)
